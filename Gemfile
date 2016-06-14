5dd9e7c4db09d4639556b772ac5b2e7c7e486a98
yoan <yoan.chapron@gmail.com>
2016-06-14 23:11:21 +0200

    add bootstrap4 & slim + quickfix on design

--- Gemfile
+++ Gemfile
@@ -37,6 +37,12 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
 
+gem 'bootstrap', '4.0.0.alpha3', github: 'twbs/bootstrap-rubygem'
+gem 'slim'
+source 'https://rails-assets.org' do
+  gem 'rails-assets-tether', '>= 1.1.0'
+end
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug'
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,21 +14,23 @@
 //= require jquery_ujs
 //= require_tree .
 
+//= require bootstrap
+
 $(document).ready(function() {
-    
+
   //Clears errors on tip form
   function clearError(category) {
     var errors = $('.error');
     for (var i = 0; i < errors.length; i++) {
       errors[i].parentNode.removeChild(errors[i]);
     }
-    
+
     var hasError = $('.has-error');
     for (var i = 0; i < hasError.length; i++) {
       hasError[i].className="form-group";
     }
   }
-  
+
   //mark the errors of the tip form
   function markError(errors, category) {
     clearError(category);
@@ -42,7 +44,7 @@ $(document).ready(function() {
       }
     }
   }
-  
+
   //Bind events on tip form
   $('.tip_form').on('ajax:success', function(e, data, status, xhr){
     var category = $(this).attr('id').replace('form-', '');
@@ -52,13 +54,13 @@ $(document).ready(function() {
     }else if (form_element.hasClass('bottom-tip-form')){
       appendTip(data, category, "bottom");
     }
-    
+
   }).on('ajax:error',function(e, xhr, status, error){
     var category = $(this).attr('id').replace('form-', '');
     markError(xhr.responseJSON, category);
   });
-  
-  
+
+
   //append a tip to a category
   function appendTip(data, category, position) {
     clearError(category);
@@ -73,7 +75,7 @@ $(document).ready(function() {
     bind_edit_events(new_tip_panel);
     $('.form-'+category+'-description').val("");
   }
-  
+
   //toggle the color of a voting button
   function toggle_color(vote_button){
     if(vote_button.hasClass('upvote_tip')){
@@ -84,12 +86,12 @@ $(document).ready(function() {
       vote_button.toggleClass('btn-danger');
     }
   }
-  
+
   //update a score by a bonus
   function update_score_value(score, bonus){
     score.text(parseInt(score.text()) + bonus);
   }
-  
+
   //udapte all tip's scores by a bonus
   function update_score(tip, bonus){
     var score = tip.find('.tip_score');
@@ -97,7 +99,7 @@ $(document).ready(function() {
     var negative_score = tip.find('.tip_negative_score');
     var upvote_button = tip.find('.upvote_tip');
     var downvote_button = tip.find('.downvote_tip');
-    
+
     if(tip.hasClass("is_not_voted")){
       update_score_value(score, bonus);
       if(bonus > 0){
@@ -109,9 +111,9 @@ $(document).ready(function() {
       }
       tip.removeClass("is_not_voted");
       tip.addClass("is_voted");
-      
+
     } else if(tip.hasClass("is_voted")){
-      
+
       //If the vote is different from the current vote
       if(upvote_button.hasClass("btn-success") && bonus < 0 || downvote_button.hasClass('btn-danger') && bonus > 0){
         update_score_value(score, 2 * bonus);
@@ -119,7 +121,7 @@ $(document).ready(function() {
         update_score_value(negative_score, -1 * bonus);
         toggle_color(upvote_button);
         toggle_color(downvote_button);
-        
+
       //If the vote is the same as the current vote
       } else {
         tip.addClass("is_not_voted");
@@ -135,7 +137,7 @@ $(document).ready(function() {
       }
     }
   }
-  
+
   //binds events on tip's voting buttons
   function bind_score_events(elements){
     elements.find('.upvote_tip').on('click', function() {
@@ -143,7 +145,7 @@ $(document).ready(function() {
       update_score(tip_panel, 1);
       event.preventDefault(); // Prevent link from following its href
     });
-    
+
     elements.find('.downvote_tip').on('click', function() {
       var tip_panel = $(this).closest('.tip_panel');
       update_score(tip_panel, -1);
@@ -152,8 +154,8 @@ $(document).ready(function() {
   }
   //actual binding of events on all tip's voting buttons
   bind_score_events($('.tip_panel'));
-  
-  
+
+
   //binds events on tip's edit button & form
   function bind_edit_events(elements){
     elements.find('.tip_edit, .cancel_edit').on('click', function(){
@@ -161,27 +163,27 @@ $(document).ready(function() {
       var tip = edit_button.closest('.tip_panel');
       var tip_body = tip.find('.tip_body');
       var edit_form = tip.find('.edit_form');
-      
+
       tip_body.toggleClass("hidden");
       edit_form.toggleClass("hidden");
     });
-    
+
     elements.find('.edit_tip').on('ajax:success', function(e, data, status, xhr){
       $(".has-error").removeClass("has-error");
       $(".error").remove();
-      
+
       var new_description = $.parseHTML(data["description"]);
       var tip_panel = $(this).closest('.tip_panel');
       var tip_description = tip_panel.find(".tip_description");
       tip_description.text("");
       tip_description.append(new_description);
-      
+
       var tip_body = tip_panel.find('.tip_body');
       var edit_form = tip_panel.find('.edit_form');
-      
+
       tip_body.toggleClass("hidden");
       edit_form.toggleClass("hidden");
-      
+
     }).on('ajax:error',function(e, xhr, status, error){
       $(".error").remove();
       var errors = xhr.responseJSON;
@@ -196,8 +198,8 @@ $(document).ready(function() {
   }
   //actual binding of events on all tip's edit buttons
   bind_edit_events($('.tip_panel'));
-  
-  
+
+
   //makes a hexagonal cel darker by hovering it
   $('.hex').hover(
     function(){
@@ -205,7 +207,7 @@ $(document).ready(function() {
       img.css('filter', 'brigthness(50%)');
       img.css('-moz-filter', 'brigthness(50%)');
       img.css('-webkit-filter', 'brightness(50%)');
-    }, 
+    },
     function() {
       var img = $(this).children().children().first();
       img.css('filter', 'brigthness(100%)');
@@ -213,25 +215,25 @@ $(document).ready(function() {
       img.css('-webkit-filter', 'brightness(100%)');
     }
   );
-  
+
   //TODO comment
   function bind_dropdown_link(){
     console.log("dropdowned!");
   };
-  
+
   //bind events on dropdown-links
   $('.dropdown-link').on('click', function bind_dropdown_link(){
     var dropdown_title = $(this);
     var dropdown_content = dropdown_title.next(".dropdown-content");
     dropdown_content.toggleClass("hidden");
-    
+
     var caret = dropdown_title.find("i.fa");
     caret.toggleClass("fa-caret-right");
     caret.toggleClass("fa-caret-down");
-    
+
   });
-  
+
   //Initialize the dropdown-contents hidden
   $('.dropdown-content').addClass("hidden");
-  
+
 });
